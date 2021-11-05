ef77d0c1830bd4273b734f935501460bf3b082c0
luisbryandr <luisbdominguezr@gmail.com>
2021-11-05 19:43:05 -0400

    added logger to dependencies and incorporated it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware} from 'redux';
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 
 import reducer from "./reducers";
@@ -11,12 +13,13 @@ import App from "./App";
 const { worker } = require('./mocks/browser');
 worker.start();
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-    <App />, 
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>,
     rootElement
 );
 
